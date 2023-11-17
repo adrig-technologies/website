e4219f45a9c0db3b0050adacceaa510515e764b8
Sreejith N Subramanian <nssreejith229@gmail.com>
2023-11-17 19:14:00 +0000

    chore: added react-icons & remove mui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { Box, Button, Container, Grid, Typography } from '@mui/material';
 import React from 'react';
 
 import Homepage from '../components/homepage/Homepage';
@@ -9,47 +8,10 @@ import TwitterCardMeta from '../components/meta/TwitterCardMeta';
 export default function Index() {
   return (
     <>
-      <Homepage />
       <BasicMeta url='/' />
       <OpenGraphMeta url='/' />
       <TwitterCardMeta url='/' />
-      <Container sx={{ height: 'inherit' }}>
-        <Grid
-          container
-          justifyContent='end'
-          alignItems='center'
-          sx={{ height: '100%' }}
-        >
-          <Grid item xs={12} textAlign='center'>
-            <Box
-              sx={{
-                //p: 0.5,
-                px: 0.3,
-                my: 1,
-                //borderRadius: 8,
-                bgcolor: 'secondary.contrastText',
-                color: 'secondary.main',
-                display: 'inline-block',
-              }}
-            >
-              <Typography fontWeight='bold' variant='subtitle1'>
-                AI TECHNOLOGIES
-              </Typography>
-            </Box>
-            <div>ADRIG</div>
-            <Typography fontWeight='normal' color='text.primary' variant='h5'>
-              We solve real world problems
-            </Typography>
-            <Button
-              sx={{ fontWeight: 'bold', mt: 2 }}
-              variant='contained'
-              color='secondary'
-            >
-              Go to Blog
-            </Button>
-          </Grid>
-        </Grid>
-      </Container>
+      <Homepage />
     </>
   );
 }
