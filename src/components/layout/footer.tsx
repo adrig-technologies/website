2b76efa9ae187e24c3d977e7c859de446d7539fe
Vagisan <96698543+Vagisan@users.noreply.github.com>
2023-11-18 20:05:53 +0000

    feat: added cards carousel footer

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='bg-gray-800 p-4 text-white'>
+      <div className='container mx-auto text-center'>
+        <p className='text-sm'>
+          &copy; 2023 Your Company. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
