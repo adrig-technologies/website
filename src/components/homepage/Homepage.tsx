579db5a6e06bd153e9c30c703910bdf570b26ef1
Sreejith N Subramanian <nssreejith229@gmail.com>
2023-11-19 20:59:48 +0000

    chore: cleaned up homepage component for fresh work

--- src/components/homepage/Homepage.tsx
+++ src/components/homepage/Homepage.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
 
-import { CardDemo } from '../layout/cards';
-import Carousel from '../layout/carousel';
-import Footer from '../layout/footer';
-import { NavigationBar } from '../layout/NavigationBar';
-import { Button } from '../ui/button';
-
-export const classNames = (...classes: string[]): string => {
-  return classes.filter(Boolean).join(' ');
-};
-
-const Example = () => {
+const Homepage = () => {
   return (
     <div className='bg-gray-800'>
-      <NavigationBar />
-      <Carousel />
-      <CardDemo />
-      <Button> Sreejith </Button>
-      <Footer />
+      {/* Homepage Components shall be added here based on the order defined in UI/UX design */}
     </div>
   );
 };
 
-export default Example;
+export default Homepage;
