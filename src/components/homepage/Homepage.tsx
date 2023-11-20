ddc9aaf644c214832df1b5288ee0e08e42c2c673
Varshith <122036366+Vachu0910@users.noreply.github.com>
2023-11-20 08:23:39 +0000

    feat: added tailwind navbar

--- src/components/homepage/Homepage.tsx
+++ src/components/homepage/Homepage.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Navbar from '../ui/navbar';
+
 const Homepage = () => {
   return (
     <div className='bg-gray-800'>
       {/* Homepage Components shall be added here based on the order defined in UI/UX design */}
+      <Navbar />
     </div>
   );
 };
