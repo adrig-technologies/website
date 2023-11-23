e74de588b2b0df79a59c71ba8d781f172db8a530
Vagisan <96698543+Vagisan@users.noreply.github.com>
2023-11-23 19:21:18 +0000

    feat: added Section

--- src/components/homepage/Homepage.tsx
+++ src/components/homepage/Homepage.tsx
@@ -1,9 +1,26 @@
+import Image from 'next/image';
 import React from 'react';
+
+import Section from '@/components/layout/Section';
 
 const Homepage = () => {
   return (
     <div className='bg-gray-800'>
       {/* Homepage Components shall be added here based on the order defined in UI/UX design */}
+      <Section
+        title='Your Title'
+        subtitle='Your Subtitle'
+        className='bg-gray-900 text-white'
+      >
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat,
+          dolor sint. Illo hic dolores aspernatur expedita atque perspiciatis
+          culpa necessitatibus exercitationem commodi corrupti quam eius
+          repudiandae accusamus rem explicabo, quia at tempore minima rerum
+          debitis quas, deserunt laboriosam dolore. Quasi?
+        </p>
+        <Image src='./' alt='Your Image' width={100} height={100} />
+      </Section>
     </div>
   );
 };
