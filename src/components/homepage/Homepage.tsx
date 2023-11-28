27f359d527212ce2f2557cea1d29e651047f358c
Sreejith N Subramanian <nssreejith229@gmail.com>
2023-11-28 22:02:09 +0000

    feat: added hero content

--- src/components/homepage/Homepage.tsx
+++ src/components/homepage/Homepage.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 import Footer from '../layout/Footer';
 import AboutUs from '../section/AboutUs';
+import HeroContent from '../section/HeroContent';
 import OurServices from '../section/OurServices';
 import Products from '../section/Products';
 import WhyChooseUs from '../section/WhyChooseUs';
-import Navbar from '../ui/navbar';
 
 const Homepage = () => {
   return (
-    <main className='bg-gray-800'>
+    <main className='bg-black'>
       {/* Homepage Components shall be added here based on the order defined in UI/UX design */}
-      <Navbar />
+      <HeroContent />
       <AboutUs />
       <WhyChooseUs />
       <OurServices />
